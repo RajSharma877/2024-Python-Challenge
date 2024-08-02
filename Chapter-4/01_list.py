93e3e9d1f9bd7c06362ca8936d6242fe963f40e9
RajSharma877 <rajsharma28440@gmail.com>
2024-08-02 22:17:06 +0530

    Completed Python

--- Chapter-4/01_list.py
+++ Chapter-4/01_list.py
@@ -4,6 +4,8 @@ print(friends[0])
 friends[0] = "Grapes"  # Unlike Strings lists are mutable
 
 print(friends[0])
-print(friends[1:4]) # We can also perform slicing in list to access range of elements from list
+print(
+    friends[1:4]
+)  # We can also perform slicing in list to access range of elements from list
 
-#We cannot perform methods on original string python creates new copy of that original string and modify that
+# We cannot perform methods on original string python creates new copy of that original string and modify that list
